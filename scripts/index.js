55f73fedfe18f4ae233b28c23218d8ef69e2330b
ncyellow <ncyellow@yandex.ru>
2023-10-05 21:22:26 +0300

    Сделана задача по требованию

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// Функция удаления карточки
+function deleteCard(event) {
+    event.target.closest('.places__item').remove();
+}
 
-// @todo: DOM узлы
+// Функция создания карточки
+function createCard(name, link, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__title').textContent = name;
 
-// @todo: Функция удаления карточки
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.setAttribute('src', link);
+    cardImage.setAttribute('alt', name);
 
-// @todo: Вывести карточки на страницу
+    const buttonDelete = cardElement.querySelector('.card__delete-button');
+    buttonDelete.addEventListener('click', deleteCard);
+
+    // по требованиям задания функция возвращает DOM элемент карточки
+    return cardElement;
+}
+
+// Вывести карточки на страницу
+function initalizeCards(cards) {
+    // Контейнер ищем тут, чтобы искать только один раз
+    const cardContainer = document.querySelector('.places__list');
+    cards.forEach(item => {
+        const cardElement = createCard(item.name, item.link, deleteCard);
+        cardContainer.appendChild(cardElement);
+    });
+}
+
+initalizeCards(initialCards);
